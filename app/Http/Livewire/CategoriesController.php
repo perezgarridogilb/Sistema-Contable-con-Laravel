307ac1796a8849304d3dfcff89bffab770be94e9
Gilberto <Betho6990@gmail.com>
2023-05-09 12:13:44 -0600

    Update Method

--- app/Http/Livewire/CategoriesController.php
+++ app/Http/Livewire/CategoriesController.php
@@ -82,6 +82,46 @@ class CategoriesController extends Component
 
     }
 
+    public function Update() {
+        $rules = [
+            'name' => "required|min:3|unique:categories,name,{$this->selected_id}",
+        ];
+
+        $messages = [
+            'name.required' => 'Nombre de categoría requerido',
+            'name.min' => 'El nombre de la categoría debe tener al menos 3 carácteres',
+            'name.unique' => 'El nombre de la categoría ya existe'
+        ];
+
+        $this->validate($rules, $messages);
+
+        $category = Category::find($this->selected_id);
+        $category->update([
+            'name' => $this->name
+        ]);
+
+        if($this->image)
+        {
+            $customFileName = uniqid() . '-.' . $this->image->extension();
+            $this->image->storeAs('public/categorias', $customFileName);
+            $imageName = $category->image;
+
+            $category->image = $customFileName;
+            $category->save();
+
+            if($imageName != null) {
+                if(file_exists('storage/categorias' . $imageName)){
+                    unlink('storage/categorias' . $imageName);
+                }
+            }
+
+            $this->resetUI();
+            $this->emit('category-updated', 'Categoría actualizada');
+            
+        }
+
+    }
+
     public function resetUI() {
         $this->name = '';
         $this->image = null;
--- resources/views/common/modalHead.blade.php
+++ resources/views/common/modalHead.blade.php
@@ -5,6 +5,6 @@
               <h5 class="modal-title text-white">
                   <b>{{$componentName}}</b> | {{ $selected_id > 0 ? 'Editar' : 'Crear' }}
               </h5>
-              <h6 class="text-center text-warning" wire:loading>POR FAVOR ESPERE</h6>
+              <h6 class="text-center text-warning" wire:loading>Por favor espere</h6>
           </div>
           <div class="modal-body">
