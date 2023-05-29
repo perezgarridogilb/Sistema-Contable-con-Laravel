372b77d553e9128e7a3cb9de188c757376baf0f8
Gilberto <Betho6990@gmail.com>
2023-05-28 23:56:33 -0600

    Part 4 Programming

--- app/Http/Livewire/PosController.php
+++ app/Http/Livewire/PosController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire;
 
 use App\Models\Denomination;
+use App\Models\Product;
 use Darryldecode\Cart\Facades\CartFacade as Cart;
 
 use Livewire\Component;
@@ -27,8 +28,8 @@ class PosController extends Component
             'denominations' => Denomination::orderBy('value', 'desc')->get(),
             'cart' => Cart::getContent()->sortBy('name')
         ])
-        ->extends('layouts.theme.app')
-        ->section('content');
+            ->extends('layouts.theme.app')
+            ->section('content');
     }
 
     public function ACash($value)
@@ -43,4 +44,62 @@ class PosController extends Component
         'clearCart' => 'clearCart',
         'saveSale' => 'saveSale'
     ];
+
+    public function Scancode($barcode, $cant = 1)
+    {
+        $product = Product::where('barcode', $barcode)->first();
+
+        if ($product == null || empty($empty)) {
+            $this->emit('scan-notfound', 'El producto no está registrado');
+        } else {
+            if ($this->inCart($product->id)) {
+                $this->increaseQty($product->id);
+                return;
+            }
+            if ($product->stock < 1) {
+                $this->emit('no-stock', 'Stock insuficiente');
+                return;
+            }
+            Cart::add($product->id, $product->name, $product->price, $cant, $product->image);
+            $this->total = Cart::getTotal();
+            $this->emit('scan-ok', 'Producto agregado');
+        }
+    }
+    /** Validar si ya existe en el carrito */
+    public function inCart($productId)
+    {
+        $exist = Cart::get($productId);
+        if ($exist) {
+            return true;
+        } else {
+            return false;
+        }
+        
+    }
+
+    public function increaseQty($productId, $cant)
+    {
+        $title = '';
+        $product = Product::find($productId);
+        $exist = Cart::get($productId);
+        if ($exist) {
+            $title = 'Cantidad actualizada';
+        } else {
+            $title = 'Producto agregado';
+        }
+
+        if ($exist) {
+            if ($product->stock < ($cant + $exist->quantity())) {
+                $this->emit('no-stock', 'Stock insuficiente');
+                return;
+            }
+        }
+
+        Cart::add($product->id, $product->name, $product->price, $cant, $product->image);
+
+        $this->total = Cart::getTotal();            
+        $this->itemsQuantity = Cart::getTotalQuatity();         
+        $this->emit('scan-ok', $title); 
+            
+    }
 }
