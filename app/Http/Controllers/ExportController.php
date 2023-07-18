47e8f7d96fd7886260d4644a54cfdca94fdc9a0f
Gilberto <Betho6990@gmail.com>
2023-07-17 23:41:27 -0600

    Testing export Excel

--- app/Http/Controllers/ExportController.php
+++ app/Http/Controllers/ExportController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Exports\SalesExport;
 use App\Models\Sale;
 use App\Models\SaleDetails;
 use App\Models\User;
@@ -18,14 +19,14 @@ class ExportController extends Controller
     public function reportPDF($userId, $reportType, $dateFrom = null, $dateTo = null) {
         $data = [];
 
-        if($this->reportType == 0) // ventas del dia
+        if($reportType == 0) // ventas del dia
         {
             $from = Carbon::parse(Carbon::now())->format('Y-m-d') . ' 00:00:00';
             $to = Carbon::parse(Carbon::now())->format('Y-m-d')   . ' 23:59:59';
 
         } else {
-             $from = Carbon::parse($this->dateFrom)->format('Y-m-d') . ' 00:00:00';
-             $to = Carbon::parse($this->dateTo)->format('Y-m-d')     . ' 23:59:59';
+             $from = Carbon::parse($dateFrom)->format('Y-m-d') . ' 00:00:00';
+             $to = Carbon::parse($dateTo)->format('Y-m-d')     . ' 23:59:59';
         }
 
 
@@ -44,9 +45,14 @@ class ExportController extends Controller
         }
 
         $user = $userId == 0 ? 'Todos' : User::find($userId)->name;
-        $pdf = PDF::loadView('pdf.reporte', compact('data', 'returnType', 'user', 'dateFrom', 'dateTo'));
+        $pdf = PDF::loadView('pdf.reporte', compact('data', 'reportType', 'user', 'dateFrom', 'dateTo'));
 
         return $pdf->stream('salesReport.pdf'); // visualiza
         return $pdf->download('salesReport.pdf'); /** descarga */
+    }
+    
+    public function reporteExcel($userId, $reportType, $dateFrom = null, $dateTo) {
+        $reportName = 'Reporte de ventas_' . uniqid() . '.xlsx';
+        return Excel::download(new SalesExport($userId, $reportType, $dateFrom, $dateTo), $reportName);
     }
 }
