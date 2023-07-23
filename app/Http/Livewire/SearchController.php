b5c4c40c8408fcbad8d353910dbda4d2d7c90a6d
Gilberto <Betho6990@gmail.com>
2023-07-22 21:50:36 -0600

    Search

--- app/Http/Livewire/SearchController.php
+++ app/Http/Livewire/SearchController.php
@@ -2,11 +2,32 @@
 
 namespace App\Http\Livewire;
 
+use App\Traits\CartTrait;
 use Livewire\Component;
+use Illuminate\Support\Str; // helper Str
 
 class SearchController extends Component
 {
+    use CartTrait;
+
     public $search;
+    public $currentPath;
+
+    public function mount() {
+        /** De esta manera obtenemos el path de donde estamos ubicados */
+        $this->currentPath = url()->current();
+    }
+
+    protected $listeners = ['scan-code' => 'ScanCode'];
+
+    public function ScanCode($barcode) {
+        /** Determinar en qué componente estamos */
+        $routeName = Str::afterLast($this->currentPath, '/');
+        if ($routeName != 'pos') {
+            $this->ScanearCode($barcode);
+            /* redirect()->to('pos'); */
+        }
+    }
     
     public function render()
     {
